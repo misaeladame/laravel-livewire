e076b42949c4a68e7c5f70d299c9c8b5329cc3a4
Misael Adame <misael_adame@protonmail.com>
2022-02-09 17:26:55 -0600

    Validacion de datos

--- app/Http/Livewire/ShowCards.php
+++ app/Http/Livewire/ShowCards.php
@@ -7,9 +7,15 @@ use App\Models\Card;
 
 class ShowCards extends Component
 {
+
+    public $search;
+
     public function render()
     {
-        $cards = Card::all();
+
+        // Misael Adame
+        $cards = Card::where('carta', 'like', '%' . $this->search . '%')
+        ->orWhere('salio', 'like', '%' . $this->search . '%')->get();
 
         return view('livewire.show-cards', compact('cards'));
     }
--- resources/views/livewire/show-cards.blade.php
+++ resources/views/livewire/show-cards.blade.php
@@ -6,56 +6,70 @@
     </x-slot>
 
 
-
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {{-- {{$posts}} --}}
         <!-- This example requires Tailwind CSS v2.0+ -->
         <x-table>
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                    <tr>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            ID</th>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Carta</th>
-                        <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Salio</th>
 
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                    </tr>
-                </thead>
+            <div class="px-6 py-4">
+                {{-- <input type="text" wire:model="search"> --}}
+                <x-jet-input class="w-full" placeholder="Escriba una carta..." type="text" wire:model="search"/>
+            </div>
 
-                <tbody class="bg-white divide-y divide-gray-200">
-                    @foreach ($cards as $card)
+
+
+            @if ($cards->count())
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
                         <tr>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{$card->id}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{$card->carta}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{$card->salio}}
-                                </div>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            </td>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                ID</th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Carta</th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Salio</th>
+
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
                         </tr>
-                    @endforeach
-                    <!-- More people... -->
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach ($cards as $card)
+                            <tr>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{$card->id}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{$card->carta}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{$card->salio}}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        <!-- More people... -->
+                    </tbody>
+                </table>
+            @else
+                <div class="px-6 py-4">
+                    No existe alguna carta con ese nombre
+                </div>
+            @endif
+
         </x-table>
     </div>
 </div>
